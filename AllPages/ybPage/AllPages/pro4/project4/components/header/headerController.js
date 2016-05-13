19dea77f8319a704d2436a4d10b455e85a57c60c
morhm <morozco2@stanford.edu>
2016-05-13 10:33:09 -0700

    les hope this works

---
+++ AllPages/ybPage/AllPages/pro4/project4/components/header/headerController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+cs142App.controller('HeaderController', ['$scope', function($scope) {
+
+   // Replace this with the code for CS142 Project #4, Problem #2
+   // console.log('window.cs142models.statesModel()', window.cs142models.statesModel());
+  $scope.head = {};
+  $scope.head.header = "Header for problem 3! : Marisa Kwiatkowski";
+  $scope.number = "num";
+
+}]);
